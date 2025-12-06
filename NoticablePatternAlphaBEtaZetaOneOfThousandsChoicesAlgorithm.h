6cb40109172a99a56ac6ebc2cbec1f3042182b6a
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-12-06 15:57:10 +0100

    Create NoticablePatternAlphaBEtaZetaOneOfThousandsChoicesAlgorithm.h

---
+++ NoticablePatternAlphaBEtaZetaOneOfThousandsChoicesAlgorithm.h
@@ -0,0 +1,7 @@
+//It would be good to save radio datas in a database while chekcing new ones in the radio network. Then (maths), the remaining checking curves must be
+//safe as many reccurent processed occur (physics) in a statisticall way. The zeta function is then the best of thos fie name three functions.
+
+class ZetaBestChecker
+{
+
+};
